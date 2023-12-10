0796893787b4ae7cd406b3a9bb3b30041fa9f9a0
Neel Smith <dnsmith.neel@gmail.com>
2023-12-10 12:02:43 -0500

    Start on citifying

---
+++ scripts/citify.jl
@@ -0,0 +1,87 @@
+# Map ebibles projects abbrevations to CITE2 work names:
+books = Dict([
+    ("Gen", "genesis"),
+    ("Ex", "exodus"),
+    ("Lev", "leviticus"),
+    ("Num", "numbers"),
+    ("DEU", "deuteronomy"),
+    ("Josh", "joshua"),
+    ("Judg", "judges"),
+    ("1SA", "samuel1"),
+    ("2SA", "samuel2"),
+    ("1KI", "kings1"),
+    ("2KI", "kings2"),
+    ("Isa", "isaiah"),
+    ("Jer", "jeremiah"),
+    ("Ezek", "ezekiel"),
+
+    ("Hos", "hosea"),
+    ("Joel", "joel"),
+    ("AMO", "amos"),
+    ("Ob", "obadiah"),
+    ("Jon", "jonah"),
+    ("Mic", "micah"),
+    ("Nah", "nahum"),
+    ("Hab", "habakkuk"),
+    ("ZEP", "zephaniah"),
+    ("Hag", "haggai"),
+    ("Zech", "zechariah"),
+    ("Mal", "malachi"),
+
+    ("Ps", "psalms"),
+    ("Prov", "proverbs"),
+    ("Job", "job"),
+    ("Song", "songs"),
+    ("Ruth", "ruth"),
+    ("Lam", "lamentations"),
+    ("Eccl", "ecclesiastes"),
+    ("Esth", "esther"),
+    ("DAN", "daniel"),
+    ("Ezra", "ezra"),
+    ("Neh", "nehemiah"),
+    ("1CH", "chronicles2"),
+    ("2CH", "chronicles2")
+
+
+    ]
+)
+
+
+
+
+
+
+
+
+
+
+ECC
+EST
+EXO
+EZE
+EZR
+GEN
+HAB
+HAG
+HOS
+ISA
+JDG
+JER
+JOB
+JOE
+JON
+JOS
+LAM
+LEV
+MAL
+MIC
+NAH
+NEH
+NUM
+OBA
+PRO
+PSA
+RUT
+SOL
+ZEC
+
