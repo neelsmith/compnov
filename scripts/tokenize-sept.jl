b597284b79730de98fa71a571dfbb91a6ff0ea3b
Neel Smith <dnsmith.neel@gmail.com>
2024-02-14 06:39:09 -0500

    Add script to tokenize Septuagint

---
+++ scripts/tokenize-sept.jl
@@ -0,0 +1,28 @@
+# Tokenize the septuagint
+repo = pwd() 
+
+
+using CitableBase, CitableCorpus, CitableText
+srcfile = joinpath(repo,"corpus", "compnov.cex")
+corpus = fromcex(srcfile, CitableTextCorpus, FileReader)
+sept = filter(corpus.passages) do p
+    versionid(p.urn) == "septuagint"
+end |> CitableTextCorpus
+
+using Orthography, PolytonicGreek
+ortho = literaryGreek()
+tkns = tokenize(sept,ortho)
+
+
+lex = filter(tkns) do tkn
+    tkn.tokentype isa LexicalToken
+end
+
+tknstrings = map(lex) do tkn
+    lowercase(tkn.passage.text)
+end
+
+using StatsBase
+using OrderedCollections
+freqs = countmap(tknstrings) |> OrderedDict
+sorted = sort(freqs, byvalue=true, rev=true)
