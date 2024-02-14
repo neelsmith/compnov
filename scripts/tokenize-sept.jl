f3faf198319ca98071387b7c4102f160ba955ef5
Neel Smith <dnsmith.neel@gmail.com>
2024-02-14 06:59:06 -0500

    Tokenize all languages in the corpus

--- scripts/tokenize-sept.jl
+++ scripts/tokenize-sept.jl
@@ -1,34 +1,67 @@
-# Tokenize the septuagint
+# Tokenize the corpora and get token frequencies.
 repo = pwd() 
 
-
 using CitableBase, CitableCorpus, CitableText
+using Orthography, PolytonicGreek, BiblicalHebrew
+using StatsBase, OrderedCollections
+
 srcfile = joinpath(repo,"corpus", "compnov.cex")
 corpus = fromcex(srcfile, CitableTextCorpus, FileReader)
+
+"""Compute token frequencies for a corpus in a given orthography, using
+a given function to normalize the token.
+"""
+function vocabfreqs(c::CitableTextCorpus, o::T, normalizer = lowercase) where T <: OrthographicSystem
+    @info("Tokenizing corpus of $(length(c.passages)) passages...")
+    tkns = tokenize(c,o)
+    @info("Done.")
+    lex = filter(tkns) do tkn
+        tkn.tokentype isa LexicalToken
+    end
+    tknstrings = map(lex) do tkn
+        normalizer(tkn.passage.text)
+    end
+    freqs = countmap(tknstrings)
+    sort(freqs, byvalue=true, rev=true)
+end
+
+
+
 sept = filter(corpus.passages) do p
     versionid(p.urn) == "septuagint"
 end |> CitableTextCorpus
 
-using Orthography, PolytonicGreek
-ortho = literaryGreek()
-tkns = tokenize(sept,ortho)
+tanach = filter(corpus.passages) do psg
+    versionid(psg.urn) == "masoretic"
+end |> CitableTextCorpus
+
+vulgate = filter(corpus.passages) do psg
+    versionid(psg.urn) == "vulgate"
+end |> CitableTextCorpus
+
+septfreqs = vocabfreqs(sept,literaryGreek())
+
+tanachfreqs = vocabfreqs(tanach,HebrewOrthography(), BiblicalHebrew.rm_accents)
 
 
-lex = filter(tkns) do tkn
-    tkn.tokentype isa LexicalToken
-end
 
-tknstrings = map(lex) do tkn
-    lowercase(tkn.passage.text)
-end
 
-using StatsBase
-using OrderedCollections
-freqs = countmap(tknstrings) |> OrderedDict
-sorted = sort(freqs, byvalue=true, rev=true)
 
-vocab = keys(sorted) |> collect
+# Need to write a Complutensian orthography!
+#vulgatefreqs = vocabfreqs(tanach,HebrewOrthography())
 
+
+
+# Write vocab lists to files:
+
+septvocab = keys(septfreqs) |> collect
 open("septuagint-vocab.txt","w") do io
-    write(io, join(vocab,"\n"))
+    write(io, join(septvocab,"\n"))
 end
+
+
+tanachvocab = keys(tanachfreqs) |> collect
+open("tanach-vocab.txt","w") do io
+    write(io, join(tanachvocab,"\n"))
+end
+
