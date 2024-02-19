15f94200a6a09f8b263b6d1dd2ec324d400faa25
Neel Smith <dnsmith.neel@gmail.com>
2024-02-19 17:00:35 -0500

    Add script for parsing septuagint

---
+++ scripts/parsesept.jl
@@ -0,0 +1,42 @@
+using Kanones, CitableParserBuilder
+using CitableBase, CitableCorpus, CitableText
+using Orthography, PolytonicGreek
+using StatsBase, OrderedCollections
+
+parserfile = "/Users/nsmith/Dropbox/_parsers/attic-2024-02-14-T0617.csv"
+parser = dfParser(parserfile)
+
+
+# Make sure thestwo directories `repo` and `kroot` have the right
+# values for the root of the compnov repository and the root of the 
+# Kanones.jl repository:
+repo = pwd()
+desk = repo |> dirname |> dirname 
+kroot = joinpath(desk, "greek-work", "Kanones.jl")
+
+
+textsrc = joinpath(repo, "corpus", "compnov.cex")
+
+corpus = fromcex(textsrc, CitableTextCorpus, FileReader)
+sept = filter(corpus.passages) do psg
+    versionid(psg.urn) == "septuagint"
+end |> CitableTextCorpus
+tkns = tokenize(sept, literaryGreek())
+
+lex = filter(tkns) do t
+    t.tokentype isa LexicalToken
+end
+
+wordforms = map(x -> Kanones.knormal(x.passage.text), lex)
+wordformfreqs = countmap(wordforms) |> OrderedDict
+
+wordformssorted = sort(wordformfreqs, byvalue = true, rev = true)
+
+vocab = keys(wordformssorted) |> collect
+
+open("sept-vocab.txt", "w") do io
+    write(io, join(vocab,"\n"))
+end
+
+
+parses = parsewordlist(vocab, parser)
